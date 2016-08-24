db4f32d6cc3f78cb1049bf7529e9326af539890e
Andres Felipe Vargas Quintana <golcarfel@gmail.com>
2016-08-24 12:15:40 -0700

    Added the mongoDB. Angular now grabs the features data directly from the mongoDB using Express and a custom Angular service.

--- Angular/app.js
+++ Angular/app.js
@@ -1,42 +1,48 @@
 var myApp = angular.module("superCoolApp", []);
 
-myApp.controller("mainCtrl", function($scope){
+myApp.controller("mainCtrl", function($scope, $interval, featureRequestService){
+    //Grab data from mongoDB
+
+    featureRequestService.getFeatures(function(dataRecieved){
+        $scope.featureList = dataRecieved.data;
+    });
+
     //Creating dummy data to fill in the page
-    $scope.featureList = [
-        {
-            featureName : "Login with Facebook",
-            points: 0,
-            comments: [],
-            usersCommented: {}
-        },
-        {
-            featureName : "Display the user’s twitter feed (handle entered in the user’s profile)",
-            points: 0,
-            comments: [],
-            usersCommented: {}
-        },
-        {
-            featureName : "Threaded comments",
-            points: 0,
-            comments: [],
-            usersCommented: {}
-        },
-        {
-            featureName : "Today’s weather drawn from the weather.com API and displayed according the user’s zip code (zipcode entered in the user’s profile)",
-            points: 0,
-            comments: [],
-            usersCommented: {}
-        },
-        {
-            featureName : "The ability to upload a profile photo",
-            points: 0,
-            comments: [],
-            usersCommented: {}
-        }
-    ];
+    // $scope.featureList = [
+    //     {
+    //         featureName : "Login with Facebook",
+    //         points: 0,
+    //         comments: [],
+    //         usersCommented: {}
+    //     },
+    //     {
+    //         featureName : "Display the user’s twitter feed (handle entered in the user’s profile)",
+    //         points: 0,
+    //         comments: [],
+    //         usersCommented: {}
+    //     },
+    //     {
+    //         featureName : "Threaded comments",
+    //         points: 0,
+    //         comments: [],
+    //         usersCommented: {}
+    //     },
+    //     {
+    //         featureName : "Today’s weather drawn from the weather.com API and displayed according the user’s zip code (zipcode entered in the user’s profile)",
+    //         points: 0,
+    //         comments: [],
+    //         usersCommented: {}
+    //     },
+    //     {
+    //         featureName : "The ability to upload a profile photo",
+    //         points: 0,
+    //         comments: [],
+    //         usersCommented: {}
+    //     }
+    // ];
 
     //Variables and function used for sorting the data
-    $scope.sortBy = "name";
+    $scope.sortBy = "featureName";
     $scope.reverseSort = false;
 
     $scope.sortFeatures = function(sortType){
@@ -63,7 +69,13 @@ myApp.controller("mainCtrl", function($scope){
 
     $scope.addComment = function (feature, comment) {
         feature.comments.push(comment);
-        this.showBox = false;
+        this.commentBoxActice = false;
         this.comment = null;
     }
+});
+
+myApp.service("featureRequestService", function ($http) {
+   this.getFeatures = function (callback) {
+       $http.get("http://127.0.0.1:3000/features").then(callback)
+   }
 });
--- Express/app.js
+++ Express/app.js
@@ -2,8 +2,17 @@ var express = require("express");
 var app = new express();
 var url = "http://127.0.0.1:3000"; //Localhost
 
+var requestFeatures = require("../MongoDB/getFeatures").requestFeatures;
+
 //Used to serve the index.html file at the root directory
 app.use(express.static("../"));
+
+app.use("/features", function (request, response) {
+    requestFeatures(function (featuresList) {
+        response.send(featuresList);
+        response.end();
+    })
+});
 
 app.listen(3000, function() {
     console.log("Server listening at " + url);
